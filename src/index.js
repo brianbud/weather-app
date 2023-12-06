53b429b5ff910f00d91bb7a9304d83ad3f38feea
Brian Bud <hello@brianbud.com>
2023-12-05 21:39:28 -0500

    webpack output management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,15 @@
+import './style.css';
+import weatherData from './weatherApi.js';
+
 function render() {
   const element = document.createElement('div');
-  element.innerHTML = `<h1>Hello World</h1>`;
+  const btn = document.createElement('button');
+  element.innerHTML = `<h1>Hello World Test</h1>`;
+  element.classList.add('test');
+
+  btn.innerHTML = 'Click me';
+  btn.onclick = weatherData;
+  element.appendChild(btn);
   return element;
 }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,31 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: {
+    index: './src/index.js',
+    weather: './src/weatherApi.js',
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'Weather App',
+    }),
+  ],
   output: {
-    filename: 'main.js',
+    filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
+    clean: true,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+      {
+        test: /\.(png|svg|jpg|jpeg|gif)$/i,
+        type: 'asset/resource',
+      },
+    ],
   },
 };
