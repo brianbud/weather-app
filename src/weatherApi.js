b6a6402e168fc19d43bbb303486f559c4ddd5dcf
Brian Bud <hello@brianbud.com>
2023-12-06 20:35:18 -0500

    convert fetch .then to async await

--- src/weatherApi.js
+++ src/weatherApi.js
@@ -1,14 +1,10 @@
-export default function getWeatherData() {
+export default async function getWeatherData() {
   console.log('get weather data');
   let inputVal = document.querySelector('input').value;
   console.log(inputVal);
-  fetch(
+  const response = await fetch(
     `http://api.weatherapi.com/v1/current.json?key=f346dd48e585413487e10743230612&q=${inputVal}`
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (response) {
-      console.log(response);
-    });
+  );
+  const data = await response.json();
+  console.log(data);
 }
